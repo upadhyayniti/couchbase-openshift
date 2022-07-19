e1035d495eeca7bb8ab0038604506efad1de19f1
upadhyayniti <niti_u@hotmail.com>
2022-07-19 15:33:47 -0400

    Updated cleanup file

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,5 @@
-oc -n kafka delete kafka --all
-oc -n kafka delete kafkatopics --all
-oc -n kafka delete subscriptions amq-streams
-oc -n kafka delete operatorgroups kafka-operatorgroup
-oc delete project kafka
+oc -n couchbase delete CouchbaseCluster --all
+oc -n couchbase delete subscriptions couchbase-enterprise-certified
+oc -n couchbase delete operatorgroups couchbase-operatorgroup
+oc delete project couchbase
 
-oc -n keycloak delete keycloakrealms --all
-oc -n keycloak delete keycloak --all
-oc -n keycloak delete subscriptions rhsso-operator
-oc -n keycloak delete operatorgroups keycloak-rhsso-operatorgroup
-oc delete project keycloak
-
-oc delete project openldap
-
-rm -rf bootstrap/roles/ocp4-install-rh-amqstreams/files/out
